e8e002d31a47edb60af762682b9af3d243f5772d
atarashansky <alexander.tarashansky@czbiohub.org>
2021-07-26 19:51:19 +0000

    pinned dependency tree

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+
 from setuptools import setup, find_packages
 
 setup(
@@ -12,16 +13,25 @@ setup(
     python_requires=">=3.7",
     # py_modules=["SAM", "utilities", "SAMGUI"],
     install_requires=[
-        "gdown",
-        "obonet",
-        "bbknn",
-        "OnClass",
-        "scvi-tools",
-        "imgkit",
-        "scanorama",
-        "scanpy",
-        "tensorflow"
+        "gdown==3.13.0",
+        "obonet==0.3.0",
+        "bbknn==1.5.1",
+        "imgkit==1.2.2",
+        "scanorama==1.7.1",
+        "scanpy==1.8.1",
+        "scvi-tools @ https://github.com/yoseflab/scvi-tools@master#egg=scvi-tools[tutorials]",
+        "OnClass @ https://github.com/wangshenguiuc/OnClass@21232f293a549a7ee0da8ebe3cbb22df3e885d4c",
+        "numpy~=1.19.2",
+        "tensorflow<2.5.0",
+        "torch==1.9.0+cu111",
+        "torchvision==0.10.0+cu111",
+        "typing-extensions~=3.7.2",
+        "importlib-metadata<2.0,>=1.0",
+        "tensorboard!=2.5.0,>=2.2.0",
+        "grpcio~=1.32.0",
+        "huggingface-hub==0.0.12"
     ],
+    dependency_links=['https://download.pytorch.org/whl/torch_stable.html'],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
