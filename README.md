4e2aba828a0fba8c4e4a950d90fc277635a860e2
atarashansky <alexander.tarashansky@czbiohub.org>
2021-07-27 00:24:32 +0000

    Update

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Create a new anaconda environment with:
 conda create -n yourenv python=3.7
 conda activate yourenv
 conda install -c anaconda -c conda-forge cudnn==8.2.1.32 cudatoolkit==11.0.221
+pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
+pip install popv
 ```
-Install popv with:
-`pip install popv`
--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 
+
 from setuptools import setup, find_packages
 
 setup(
     name="popv",
-    version="0.0.2",
+    version="0.0.4",
     description="The PopV algorithm",
     long_description="The PopV algorithm for unsupervised label transfer from reference datasets.",
     long_description_content_type="text/markdown",
@@ -19,21 +20,18 @@ setup(
         "imgkit==1.2.2",
         "scanorama==1.7.1",
         "scanpy==1.8.1",
-        "scvi-tools @ https://github.com/yoseflab/scvi-tools@master#egg=scvi-tools[tutorials]",
-        "OnClass @ https://github.com/wangshenguiuc/OnClass@21232f293a549a7ee0da8ebe3cbb22df3e885d4c",
+        "scvi-tools",
+        "OnClass",
         "numpy~=1.19.2",
         "tensorflow<2.5.0",
-        "torch==1.9.0+cu111",
-        "torchvision==0.10.0+cu111",
-        "typing-extensions~=3.7.2",
         "importlib-metadata<2.0,>=1.0",
         "tensorboard!=2.5.0,>=2.2.0",
         "grpcio~=1.32.0",
         "huggingface-hub==0.0.12",
         "tqdm>=4.56.0",
-        "six~=1.15.0"
+        "six~=1.15.0",
+        "typing-extensions~=3.7.4"
     ],
-    dependency_links=['https://download.pytorch.org/whl/torch_stable.html'],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
